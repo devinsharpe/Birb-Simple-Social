9e85fd78ca4d461ea5983a0c57479f09f8e51c1b
Devin Sharpe <31522303+devinsharpe@users.noreply.github.com>
2023-01-04 04:32:13 -0500

    chore: add placeholder pages

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import React from "react";
+
+const PrivacyPage: NextPage = () => {
+  return <div>PrivacyPage</div>;
+};
+
+export default PrivacyPage;
---
+++ src/pages/terms.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import React from "react";
+
+const TermsPage: NextPage = () => {
+  return <div>TermsPage</div>;
+};
+
+export default TermsPage;
