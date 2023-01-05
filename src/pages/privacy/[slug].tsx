fd310357d04c388a267430a17d62223113f6a952
Devin Sharpe <31522303+devinsharpe@users.noreply.github.com>
2023-01-05 04:17:18 -0500

    feat: add privacy policy page and logic

---
+++ src/pages/privacy/[slug].tsx
@@ -0,0 +1,72 @@
+import type { DocsPageProps, SlugParams } from "../../utils/docs";
+import type { GetStaticProps, NextPage } from "next";
+import { cleanFilename, getDocs, processMarkdownFile } from "../../utils/docs";
+
+import DocsNav from "../../components/docs/nav";
+import React from "react";
+import { useRouter } from "next/router";
+
+const PrivacyPage: NextPage<DocsPageProps> = ({
+  content,
+  data,
+  docs,
+  slug,
+}) => {
+  const router = useRouter();
+  return (
+    <>
+      <DocsNav
+        current={slug}
+        docs={docs}
+        onDocSelect={(doc) => router.push(`/privacy/${doc}`)}
+      />
+      <div className="mt-16 px-8 py-16">
+        <h1 className="text-3xl font-bold tracking-wide">
+          Birb Social Privacy Policy
+        </h1>
+      </div>
+      <div className="mx-8 rounded-md bg-zinc-200 p-4 dark:bg-zinc-800">
+        <p>
+          Effective: {new Date(data.effectiveDate * 1000).toLocaleDateString()}
+        </p>
+        <p>Posted: {new Date(data.createdDate * 1000).toLocaleDateString()}</p>
+      </div>
+      <div
+        dangerouslySetInnerHTML={{ __html: content }}
+        className="prose prose-zinc p-8 dark:prose-invert lg:max-w-4xl lg:prose-lg xl:prose-xl"
+      />
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps<
+  DocsPageProps,
+  SlugParams
+> = async ({ params }) => {
+  const files = await getDocs("privacy");
+  let filename: string;
+  if (params?.slug && files.includes(params.slug + ".md"))
+    filename = params.slug + ".md";
+  else filename = files[0];
+  return {
+    props: {
+      ...(await processMarkdownFile(filename, "privacy")),
+      docs: files.map((f) => cleanFilename(f)),
+      slug: params?.slug,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const files = await getDocs("privacy");
+  return {
+    paths: files.map((file) => ({
+      params: {
+        slug: cleanFilename(file),
+      },
+    })),
+    fallback: false,
+  };
+};
+
+export default PrivacyPage;
