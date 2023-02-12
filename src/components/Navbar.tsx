c8f631e4b5cb41df72c3c1bc516aa397a8b0c662
Devin Sharpe <devinrsharpe@gmail.com>
2023-02-12 01:36:42 -0500

    feat: add login modal and tweaks to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,81 +3,91 @@ import { signOut, useSession } from "next-auth/react";
 import DialogMenu from "./DialogMenu";
 import FeatherIcon from "feather-icons-react";
 import Image from "next/image";
+import LoginModal from "./modals/login";
 import React from "react";
-import { useRouter } from "next/router";
+import atoms from "../atoms";
+import { useSetAtom } from "jotai";
 
 const Navbar = () => {
-  const router = useRouter();
+  const setModal = useSetAtom(atoms.modal);
   const session = useSession();
   return (
-    <nav className="mt-4 flex h-10 w-full justify-center">
-      <div className="container flex items-center justify-between gap-4 px-4">
-        <Image
-          src={"icons/icon.svg"}
-          fill={false}
-          width={36}
-          height={36}
-          alt="Birb Logo"
-        />
-        <div className="flex items-center gap-4 text-zinc-600 dark:text-zinc-300">
-          <button
-            type="button"
-            aria-label="Search button"
-            className="hover:text-zinc-800 dark:hover:text-white"
-          >
-            <FeatherIcon icon="search" size={24} />
-          </button>
-          <DialogMenu
-            items={[
-              [
-                {
-                  icon: "home",
-                  text: "Home",
-                  onClick: console.log,
-                },
-                {
-                  icon: "users",
-                  text: "Circles",
-                  onClick: console.log,
-                },
-                {
-                  icon: "bell",
-                  text: "Notifications",
-                  onClick: console.log,
-                },
-                {
-                  icon: "message-circle",
-                  text: "Messages",
-                  onClick: console.log,
-                },
-                {
-                  icon: "user",
-                  text: "Profile",
-                  onClick: console.log,
-                },
-              ],
-              [
-                {
-                  icon: "settings",
-                  text: "Settings",
-                  onClick: console.log,
-                },
-                {
-                  icon:
-                    session.status === "authenticated" ? "log-out" : "log-in",
-                  text:
-                    session.status === "authenticated" ? "Log Out" : "Log In",
-                  onClick:
-                    session.status === "authenticated"
-                      ? signOut
-                      : () => router.push("/login"),
-                },
-              ],
-            ]}
+    <>
+      <nav className="mt-4 flex h-10 w-full justify-center">
+        <div className="container flex items-center justify-between gap-4 px-4">
+          <Image
+            src={"icons/icon.svg"}
+            fill={false}
+            width={36}
+            height={36}
+            alt="Birb Logo"
           />
+          <div className="flex items-center gap-4 ">
+            <button
+              type="button"
+              aria-label="Search button"
+              className="rounded-lg p-1 text-zinc-600 hover:text-zinc-800 dark:text-zinc-300 dark:hover:text-white"
+            >
+              <FeatherIcon icon="search" size={24} />
+            </button>
+            <DialogMenu
+              items={[
+                [
+                  {
+                    icon: "home",
+                    text: "Home",
+                    onClick: console.log,
+                  },
+                  {
+                    icon: "users",
+                    text: "Circles",
+                    onClick: console.log,
+                    disabled: session.status !== "authenticated",
+                  },
+                  {
+                    icon: "bell",
+                    text: "Notifications",
+                    onClick: console.log,
+                    disabled: session.status !== "authenticated",
+                  },
+                  {
+                    icon: "message-circle",
+                    text: "Messages",
+                    onClick: console.log,
+                    disabled: session.status !== "authenticated",
+                  },
+                  {
+                    icon: "user",
+                    text: "Profile",
+                    onClick: console.log,
+                    disabled: session.status !== "authenticated",
+                  },
+                ],
+                [
+                  {
+                    icon: "settings",
+                    text: "Settings",
+                    onClick: console.log,
+                    disabled: session.status !== "authenticated",
+                  },
+                  {
+                    icon:
+                      session.status === "authenticated" ? "log-out" : "log-in",
+                    text:
+                      session.status === "authenticated" ? "Log Out" : "Log In",
+                    onClick:
+                      session.status === "authenticated"
+                        ? signOut
+                        : () => setModal("login"),
+                  },
+                ],
+              ]}
+            />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <LoginModal />
+    </>
   );
 };
 
