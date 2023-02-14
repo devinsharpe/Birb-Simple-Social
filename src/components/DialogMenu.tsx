346a8081d61f0a7b3f8f6e99b543ab11d869ce89
Devin Sharpe <devinrsharpe@gmail.com>
2023-02-14 01:38:00 -0500

    feat: add dialog menu button props to dialog menu

--- src/components/DialogMenu.tsx
+++ src/components/DialogMenu.tsx
@@ -32,25 +32,40 @@ const DialogMenuItem: React.FC<DialogMenuItemProps> = ({
           <FeatherIcon
             icon={icon}
             size={20}
-            className={`${!disabled && "dark:group-hover:text-violet-400"}`}
+            className={`shrink-0 ${
+              !disabled && "dark:group-hover:text-violet-400"
+            }`}
           />
-          <span>{text}</span>
+          <span className="whitespace-nowrap">{text}</span>
         </button>
       )}
     </Menu.Item>
   );
 };
 
+const DialogMenuButton: React.FC<{
+  children: JSX.Element;
+  className?: string;
+}> = ({ children, className }) => {
+  return (
+    <Menu.Button
+      className={`z-[1] inline-flex w-full shrink-0 justify-center px-4 py-2 text-zinc-600 hover:text-zinc-800 dark:text-zinc-300 dark:hover:text-white ${className}`}
+    >
+      {children}
+    </Menu.Button>
+  );
+};
+
 const DialogMenu: React.FC<{
+  children: JSX.Element;
+  className?: string;
   items: (DialogMenuItemProps | DialogMenuItemProps[])[];
-}> = ({ items }) => {
+}> = ({ children, className, items }) => {
   return (
     <div className="text-right">
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button className="inline-flex w-full justify-center px-4 py-2 text-zinc-600 hover:text-zinc-800 dark:text-zinc-300 dark:hover:text-white">
-            <FeatherIcon icon="menu" size={24} aria-hidden="true" />
-          </Menu.Button>
+          <DialogMenuButton className={className}>{children}</DialogMenuButton>
         </div>
         <Transition
           as={Fragment}
@@ -61,7 +76,7 @@ const DialogMenu: React.FC<{
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-zinc-400 rounded border border-zinc-400 bg-white py-1 shadow-lg focus:outline-none dark:divide-zinc-700 dark:border-zinc-700 dark:bg-zinc-800">
+          <Menu.Items className="absolute right-0 z-10 mt-2 w-auto min-w-[14rem] origin-top-right divide-y divide-zinc-400 rounded border border-zinc-400 bg-white py-1 shadow-lg focus:outline-none dark:divide-zinc-700 dark:border-zinc-700 dark:bg-zinc-800">
             {items.map((item, index) =>
               Array.isArray(item) ? (
                 <div className="px-2 py-1" key={index}>
