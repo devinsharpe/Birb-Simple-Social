7d7d362400a0c5155af8eacd8bd380e9b3cb29e8
Devin Sharpe <31522303+devinsharpe@users.noreply.github.com>
2023-02-15 09:44:04 -0500

    feat: change default font to Inter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   darkMode: "media",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--inter-font)", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
