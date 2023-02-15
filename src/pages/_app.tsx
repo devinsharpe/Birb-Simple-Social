cb7f8fdd04a44d8bcf538e2c59ed881100500e2e
Devin Sharpe <31522303+devinsharpe@users.noreply.github.com>
2023-02-15 09:45:01 -0500

    chore: configure inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { Inter } from "@next/font/google";
 
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 import React from "react";
 import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -18,7 +21,16 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <link rel="icon" href="/icons/icon.svg" />
       </Head>
       <SessionProvider session={session}>
-        <Component {...pageProps} />
+        <style jsx global>
+          {`
+            :root {
+              --inter-font: ${inter.style.fontFamily};
+            }
+          `}
+        </style>
+        <main className="min-h-screen">
+          <Component {...pageProps} />
+        </main>
       </SessionProvider>
     </>
   );
