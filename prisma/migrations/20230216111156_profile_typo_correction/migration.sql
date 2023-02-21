24eac3912602b669f122d527044dcfde9c8203a3
Devin Sharpe <devinrsharpe@gmail.com>
2023-02-20 23:39:50 -0500

    fix: add search to profile model

---
+++ prisma/migrations/20230216111156_profile_typo_correction/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Profile" ALTER COLUMN "headerUrl" SET DEFAULT 'https://source.unsplash.com/random/1920×1080/?cat',
+ALTER COLUMN "avatarUrl" SET DEFAULT 'https://source.unsplash.com/random/600×600/?cat';
