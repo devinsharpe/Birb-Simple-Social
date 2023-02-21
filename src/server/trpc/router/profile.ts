c5b0de4c2b704f7957b2296fb56a182d2949ace0
Devin Sharpe <devinrsharpe@gmail.com>
2023-02-20 23:54:17 -0500

    feat: add profile trpc actions

--- src/server/trpc/router/profile.ts
+++ src/server/trpc/router/profile.ts
@@ -2,8 +2,9 @@ import { names, uniqueNamesGenerator } from "unique-names-generator";
 import { protectedProcedure, router } from "../trpc";
 
 import type { Config } from "unique-names-generator";
+import { z } from "zod";
 
-const nameConfig: Config = {
+export const nameConfig: Config = {
   dictionaries: [
     [
       "Affectionate",
@@ -61,15 +62,31 @@ const nameConfig: Config = {
 };
 
 export const profileRouter = router({
+  checkProfileHandle: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      const userProfile = await ctx.prisma.profile.findFirst({
+        where: {
+          id: ctx.session.user.id,
+        },
+      });
+      const handleProfile = await ctx.prisma.profile.findFirst({
+        where: {
+          handle: input.toLowerCase(),
+        },
+      });
+      if (handleProfile && userProfile && handleProfile.id === userProfile.id)
+        return true;
+      else return !handleProfile;
+    }),
   getProfile: protectedProcedure.query(async ({ ctx }) => {
-    const profile = ctx.prisma.profile.findFirst({
+    const profile = await ctx.prisma.profile.findFirst({
       where: {
         id: ctx.session.user.id,
       },
     });
 
-    if (typeof profile !== "undefined") {
-      console.log("Hello world");
+    if (profile) {
       return profile;
     } else {
       const result = await ctx.prisma.profile.create({
@@ -78,10 +95,58 @@ export const profileRouter = router({
           name: "",
           handle: uniqueNamesGenerator(nameConfig)
             .toLowerCase()
-            .replaceAll(" ", "-"),
+            .replaceAll("_", "-"),
         },
       });
       return result;
     }
   }),
+  searchProfiles: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      const result = await ctx.prisma.profile.findMany({
+        take: 10,
+        where: {
+          handle: {
+            search: input,
+          },
+          name: {
+            search: input,
+          },
+        },
+      });
+      return result;
+    }),
+  updateProfile: protectedProcedure
+    .input(
+      z.strictObject({
+        name: z.string().max(50),
+        handle: z.string().max(24),
+        biography: z.string().max(150),
+        birthdate: z.string().max(10),
+        location: z.string().max(50),
+        website: z.string(),
+        avatarUrl: z.string(),
+        headerUrl: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const oldProfile = await ctx.prisma.profile.findFirst({
+        where: { id: ctx.session.user.id },
+      });
+      if (oldProfile) {
+        const profile = await ctx.prisma.profile.update({
+          where: {
+            id: ctx.session.user.id,
+          },
+          data: {
+            ...input,
+            canChangeHandle:
+              input.handle === oldProfile.handle && oldProfile.canChangeHandle,
+          },
+        });
+        return profile;
+      }
+      return oldProfile;
+    }),
 });
