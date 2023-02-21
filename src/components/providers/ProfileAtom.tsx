cd12dcc816923ef386e41efe57a6a66e52ec23be
Devin Sharpe <devinrsharpe@gmail.com>
2023-02-20 23:52:26 -0500

    fix: update profile atom provider

--- src/components/providers/ProfileAtom.tsx
+++ src/components/providers/ProfileAtom.tsx
@@ -2,21 +2,18 @@ import React, { useEffect } from "react";
 
 import atoms from "../../atoms";
 import { trpc } from "../../utils/trpc";
-import { useSession } from "next-auth/react";
 import { useSetAtom } from "jotai";
 
 const ProfileAtomProvider = () => {
   const setProfile = useSetAtom(atoms.profile);
-  const session = useSession();
   const profile = trpc.profiles.getProfile.useQuery(undefined);
-  setProfile(profile.data || undefined);
-  console.log(profile.data);
+  const setModal = useSetAtom(atoms.modal);
+  useEffect(() => {
+    setProfile(profile.data || undefined);
+    if (profile.data?.canChangeHandle) setModal("welcome");
+  }, [profile]);
 
-  return (
-    <div>
-      <p>Profile Provider</p>
-    </div>
-  );
+  return null;
 };
 
 export default ProfileAtomProvider;
