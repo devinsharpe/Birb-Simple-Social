fc85d1c9fa95b40c0e84e85cffb2c4a40d713ef5
Devin Sharpe <devinrsharpe@gmail.com>
2023-03-01 01:21:17 -0500

    feat: update handle page

--- src/pages/@/[handle].tsx
+++ src/pages/@/[handle].tsx
@@ -1,13 +1,20 @@
-import { GetServerSideProps, NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
+import type {
+  Profile,
+  ProfileRelationship,
+  RelationshipRequest,
+} from "@prisma/client";
+import React, { useCallback, useEffect, useState } from "react";
+import { RelationshipType, RequestStatus } from "@prisma/client";
 import { useAtomValue, useSetAtom } from "jotai";
 
 import FeatherIcon from "feather-icons-react";
+import Image from "next/image";
 import LoginPrompt from "../../components/LoginPrompt";
 import Navbar from "../../components/Navbar";
-import { Profile } from "@prisma/client";
-import React from "react";
 import atoms from "../../atoms";
 import { prisma } from "../../server/db/client";
+import { trpc } from "../../utils/trpc";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 
@@ -15,6 +22,60 @@ interface PageProps {
   handle: string;
   profile: Profile | null;
 }
+
+const ProfileActionButton: React.FC<{
+  isUser: boolean;
+  isFollowing: boolean;
+  hasRequest: boolean;
+  onCancel: () => void;
+  onEditClick: () => void;
+  onFollow: () => void;
+}> = ({ isUser, isFollowing, hasRequest, onCancel, onEditClick, onFollow }) => {
+  if (isUser) {
+    return (
+      <button
+        type="button"
+        className="flex items-center gap-2 rounded-full bg-violet-600 px-6 py-2 text-white hover:bg-violet-700 dark:hover:bg-violet-500"
+        onClick={onEditClick}
+      >
+        <FeatherIcon icon="user" size={20} />
+        <span>Edit</span>
+      </button>
+    );
+  } else if (isFollowing) {
+    return (
+      <button
+        type="button"
+        className="flex items-center gap-2 rounded-full bg-zinc-600 px-6 py-2 text-white hover:bg-zinc-700 dark:hover:bg-zinc-500"
+      >
+        <FeatherIcon icon="zap" size={20} />
+        <span>Following</span>
+      </button>
+    );
+  } else if (hasRequest) {
+    return (
+      <button
+        type="button"
+        className="flex items-center gap-2 rounded-full bg-zinc-600 px-6 py-2 text-white hover:bg-zinc-700 dark:hover:bg-zinc-500"
+        onClick={onCancel}
+      >
+        <FeatherIcon icon="clock" size={20} />
+        <span>Pending</span>
+      </button>
+    );
+  } else {
+    return (
+      <button
+        type="button"
+        className="flex items-center gap-2 rounded-full bg-violet-600 px-6 py-2 text-white hover:bg-violet-700 dark:hover:bg-violet-500"
+        onClick={onFollow}
+      >
+        <FeatherIcon icon="zap" size={20} />
+        <span>Follow</span>
+      </button>
+    );
+  }
+};
 
 const BlankHeader: React.FC<{
   handle: string;
@@ -24,18 +85,22 @@ const BlankHeader: React.FC<{
     <>
       <div className="relative mx-auto mb-16 pt-4">
         <div className="relative aspect-[7/3] w-full overflow-hidden sm:rounded-md">
-          <img
+          <Image
             src="https://source.unsplash.com/random/1920×1080/?cat"
             alt={`Missing profile's header image`}
             className="h-full w-full object-cover object-center grayscale"
+            width={1080}
+            height={2520}
           />
         </div>
 
         <div className="absolute inset-x-0 -bottom-12 left-1/2 z-[1] h-28 w-28 -translate-x-1/2 transform overflow-hidden rounded-full border-4 border-white object-center dark:border-zinc-800">
-          <img
+          <Image
             src="https://source.unsplash.com/random/600×600/?cat"
             alt="Missing profile's avatar image"
             className="h-full w-full object-cover object-center grayscale"
+            width={256}
+            height={256}
           />
         </div>
       </div>
@@ -48,7 +113,7 @@ const BlankHeader: React.FC<{
 
       <div className="pt-12">
         <h4 className="text-center text-2xl font-bold text-black dark:text-white md:text-4xl">
-          This account doesn't exist
+          This account doesn&apos;t exist
         </h4>
         <h5 className="text-center text-xl font-medium text-zinc-700 dark:text-zinc-400 md:text-2xl">
           It seems like not everyone is on Birb yet
@@ -69,32 +134,48 @@ const BlankHeader: React.FC<{
 };
 
 const ProfileHeader: React.FC<{
-  isEditable?: boolean;
+  onCancelClick: () => void;
   onEditClick: () => void;
+  onFollowClick: () => void;
   profile: Profile;
-}> = ({ isEditable = false, onEditClick, profile }) => {
+  isUser: boolean;
+  isFollowing: boolean;
+  hasRequest: boolean;
+}> = ({
+  onCancelClick,
+  onEditClick,
+  onFollowClick,
+  profile,
+  isUser,
+  isFollowing,
+  hasRequest,
+}) => {
   return (
     <>
       <div className="relative mx-auto mb-16 pt-4">
         <div className="relative aspect-[7/3] w-full overflow-hidden sm:rounded-md">
-          <img
+          <Image
             src={
               profile.headerUrl ||
               "https://source.unsplash.com/random/600×600/?cat"
             }
             alt={`${profile.name}'s header image`}
             className="h-full w-full object-cover object-center"
+            width={2520}
+            height={1080}
           />
         </div>
 
         <div className="absolute inset-x-0 -bottom-12 left-1/2 z-[1] h-28 w-28 -translate-x-1/2 transform overflow-hidden rounded-full border-4 border-white object-center dark:border-zinc-800">
-          <img
+          <Image
             src={
               profile.avatarUrl ||
               "https://source.unsplash.com/random/600×600/?cat"
             }
             alt={`${profile.name}'s avatar image`}
             className="h-full w-full object-cover object-center"
+            width={256}
+            height={256}
           />
         </div>
       </div>
@@ -106,24 +187,14 @@ const ProfileHeader: React.FC<{
             @{profile.handle}
           </h3>
         </div>
-        {isEditable ? (
-          <button
-            type="button"
-            className="flex items-center gap-2 rounded-full bg-violet-600 px-6 py-2 text-white hover:bg-violet-700 dark:hover:bg-violet-500"
-            onClick={onEditClick}
-          >
-            <FeatherIcon icon="user" size={20} />
-            <span>Edit</span>
-          </button>
-        ) : (
-          <button
-            type="button"
-            className="flex items-center gap-2 rounded-full bg-violet-600 px-6 py-2 text-white hover:bg-violet-700 dark:hover:bg-violet-500"
-          >
-            <FeatherIcon icon="zap" size={20} />
-            <span>Follow</span>
-          </button>
-        )}
+        <ProfileActionButton
+          isUser={isUser}
+          isFollowing={isFollowing}
+          hasRequest={hasRequest}
+          onCancel={onCancelClick}
+          onEditClick={onEditClick}
+          onFollow={onFollowClick}
+        />
       </div>
       <p className="px-4 py-2 text-zinc-800 dark:text-zinc-200">
         {profile.biography}
@@ -160,16 +231,54 @@ const ProfilePage: NextPage<PageProps> = ({ handle, profile }) => {
   const session = useSession();
   const userProfile = useAtomValue(atoms.profile);
   const setModal = useSetAtom(atoms.modal);
-  console.log(profile);
+
+  const [relationship, setRelationship] = useState<ProfileRelationship | null>(
+    null
+  );
+  const [request, setRequest] = useState<RelationshipRequest | null>(null);
+
+  const cancelFollow = trpc.profiles.cancelFollow.useMutation();
+  const followProfile = trpc.profiles.requestFollow.useMutation();
+  const getRelationship = trpc.profiles.getRelationship.useMutation();
+
+  useEffect(() => {
+    if (profile && userProfile) {
+      getRelationship.mutateAsync(profile.id).then((response) => {
+        setRelationship(response.relationship);
+        setRequest(response.request);
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [profile, userProfile]);
+
+  const handleCancelClick = useCallback(async () => {
+    if (request) {
+      await cancelFollow.mutateAsync(request.id);
+      setRequest(null);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [request]);
+
+  const handleFollowClick = useCallback(async () => {
+    if (!request && !relationship && profile) {
+      const newRequest = await followProfile.mutateAsync(profile.id);
+      console.log(newRequest);
+      if (newRequest) setRequest(newRequest);
+    }
+  }, [request, relationship, profile, followProfile]);
 
   return (
     <>
       <section className="hide-scrollbar container mx-auto h-screen max-w-2xl overflow-y-scroll py-16 ">
         {profile ? (
           <ProfileHeader
-            isEditable={userProfile && userProfile.id === profile.id}
+            onCancelClick={handleCancelClick}
             onEditClick={() => setModal("profile-edit")}
+            onFollowClick={handleFollowClick}
             profile={profile}
+            isUser={!!userProfile && profile.id === userProfile.id}
+            isFollowing={!!relationship}
+            hasRequest={!!request}
           />
         ) : (
           <BlankHeader handle={handle} />
@@ -184,11 +293,23 @@ const ProfilePage: NextPage<PageProps> = ({ handle, profile }) => {
 export const getServerSideProps: GetServerSideProps<PageProps> = async (
   context
 ) => {
-  let profile = null;
+  let profile: Profile | null = null;
   if (context.params && context.params?.handle) {
     profile = await prisma.profile.findFirst({
       where: {
         handle: context.params.handle as string,
+      },
+      include: {
+        followers: {
+          where: {
+            type: RelationshipType.FOLLOW,
+          },
+        },
+        following: {
+          where: {
+            type: RelationshipType.FOLLOW,
+          },
+        },
       },
     });
   }
