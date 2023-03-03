523d842eba9a4e3064845ccd3c70bf842b8db407
Devin Sharpe <devinrsharpe@gmail.com>
2023-03-03 07:04:19 -0500

    feat: setup tensorflow

--- moderation/src/server/tensorflow.ts
+++ moderation/src/server/tensorflow.ts
@@ -1,4 +1,17 @@
-import "@tensorflow/tfjs"
-import toxicity from "@tensorflow-models/toxicity"
+import "@tensorflow/tfjs";
+import * as toxicity from "@tensorflow-models/toxicity";
+import { env } from "../env/server.mjs";
 
+const txjs = async () => {
+  const model = await toxicity.load(env.TOXICITY_THRESHOLD * 0.1, [
+    "insult",
+    "obscene",
+    "severe_toxicity",
+    "sexual_explicit",
+    "threat",
+    "toxicity"
+  ]);
+  return model;
+};
 
+export default txjs();
