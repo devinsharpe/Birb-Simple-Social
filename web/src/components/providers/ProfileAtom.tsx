c3c4885c3bba9cf1ee1fb7387e248ac492b95b8f
Devin Sharpe <devinrsharpe@gmail.com>
2023-03-12 00:08:12 -0500

    fix: remove profile atom welcome modal call

--- web/src/components/providers/ProfileAtom.tsx
+++ web/src/components/providers/ProfileAtom.tsx
@@ -3,7 +3,6 @@ import { trpc } from "../../utils/trpc";
 import { useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useSetAtom } from "jotai";
-import { KEY as WELCOME_KEY } from "../modals/Welcome";
 
 const ProfileAtomProvider = () => {
   const setProfile = useSetAtom(atoms.profile);
@@ -15,7 +14,6 @@ const ProfileAtomProvider = () => {
 
   useEffect(() => {
     setProfile(profile.data || undefined);
-    if (profile.data?.canChangeHandle) setModal(WELCOME_KEY);
   }, [profile, setModal, setProfile]);
 
   return null;
