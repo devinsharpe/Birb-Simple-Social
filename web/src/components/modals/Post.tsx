8d59053861ba49c0a597af84f50494651a3ee9dd
Devin Sharpe <devinrsharpe@gmail.com>
2023-03-15 17:49:09 -0400

    feat: add submit logic and indicator to post modal

--- web/src/components/modals/Post.tsx
+++ web/src/components/modals/Post.tsx
@@ -1,8 +1,12 @@
-import { useAtom } from "jotai";
-import { useEffect, useState } from "react";
-import atoms from "../../atoms";
+import { useCallback, useEffect, useState } from "react";
+
 import DialogModal from "../DialogModal";
+import FeatherIcon from "feather-icons-react";
 import PostForm from "../forms/Post";
+import { PostType } from "@prisma/client";
+import atoms from "../../atoms";
+import { trpc } from "../../utils/trpc";
+import { useAtom } from "jotai";
 
 export const KEY = "post-new";
 
@@ -11,15 +15,55 @@ export interface SimplePost {
   location: string;
 }
 
+interface AvailablePostIndicatorProps {
+  style: "violet" | "zinc";
+  type: PostType;
+  value: number;
+}
+
+const AvailablePostIndicator: React.FC<AvailablePostIndicatorProps> = ({
+  style,
+  type,
+  value,
+}) => {
+  return (
+    <>
+      {[...Array(value).keys()].map((index) => (
+        <div
+          key={`avail-post-${type}-${style}-${index}`}
+          className={`flex h-8 w-8 items-center justify-center rounded-full border ${
+            style === "violet"
+              ? "border-violet-300 bg-violet-100 text-violet-700 dark:border-violet-600 dark:bg-violet-700 dark:text-violet-200"
+              : "border-zinc-300 bg-zinc-100 text-zinc-500 dark:border-zinc-600 dark:bg-zinc-700"
+          }`}
+        >
+          {type === PostType.TEXT ? (
+            <FeatherIcon size={20} icon="align-left" />
+          ) : (
+            <FeatherIcon size={16} icon="image" />
+          )}
+        </div>
+      ))}
+    </>
+  );
+};
+
 const PostModal = () => {
+  const [available, setAvailable] = useState({
+    image: 0,
+    text: 0,
+  });
   const [post, setPost] = useState<SimplePost>({
     text: "",
     location: "",
   });
   const [modal, setModal] = useAtom(atoms.modal);
+  const createPost = trpc.posts.create.useMutation();
+  const getAvaiable = trpc.posts.availableCount.useMutation();
 
   useEffect(() => {
     if (!modal) {
+      getAvaiable.mutateAsync().then((avail) => setAvailable(avail));
       setPost({
         text: "",
         location: "",
@@ -27,14 +71,54 @@ const PostModal = () => {
     }
   }, [modal]);
 
+  const handleSubmit = useCallback(async () => {
+    if (post.text) {
+      const newPost = await createPost.mutateAsync({
+        ...post,
+        type: PostType.TEXT,
+      });
+      setModal(undefined);
+      return newPost;
+    }
+    return null;
+  }, [post.text, post.location]);
+
   return (
     <DialogModal isDismissable={false} name={KEY} title="Create Post">
-      <PostForm
-        post={post}
-        onCancel={() => setModal(undefined)}
-        onChange={(val) => setPost(val)}
-        onSubmit={console.log}
-      />
+      <>
+        <div
+          className="absolute top-4 right-4 z-[3] flex cursor-pointer items-center justify-end -space-x-4"
+          title={`${available.text} text posts avaible, ${available.image} image posts avaible`}
+        >
+          <AvailablePostIndicator
+            style="zinc"
+            type={PostType.TEXT}
+            value={3 - available.text}
+          />
+          <AvailablePostIndicator
+            style="violet"
+            type={PostType.TEXT}
+            value={available.text}
+          />
+          <AvailablePostIndicator
+            style="zinc"
+            type={PostType.IMAGE}
+            value={1 - available.image}
+          />
+          <AvailablePostIndicator
+            style="violet"
+            type={PostType.IMAGE}
+            value={available.image}
+          />
+        </div>
+        <PostForm
+          post={post}
+          isLoading={createPost.isLoading}
+          onCancel={() => setModal(undefined)}
+          onChange={(val) => setPost(val)}
+          onSubmit={handleSubmit}
+        />
+      </>
     </DialogModal>
   );
 };
