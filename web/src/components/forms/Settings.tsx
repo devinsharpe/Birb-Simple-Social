3e01c110bf00613c50622fb5b03cd59e8207946e
Devin Sharpe <devinrsharpe@gmail.com>
2023-04-04 14:58:57 -0400

    feat: add settings form and functionality

---
+++ web/src/components/forms/Settings.tsx
@@ -0,0 +1,201 @@
+import { ProfileSettings, Reaction, Theme } from "@prisma/client";
+
+import FeatherIcon from "feather-icons-react";
+import React from "react";
+import { Switch } from "@headlessui/react";
+
+type OptionalIdSettings = Omit<ProfileSettings, "id">;
+
+interface SettingsFormProps {
+  settings: OptionalIdSettings;
+  onChange: (settings: OptionalIdSettings) => void;
+  onDeleteAccount: () => void;
+  onDownloadUserData: () => void;
+  onReset: () => void;
+  onSubmit: () => void;
+}
+
+const SettingsForm: React.FC<SettingsFormProps> = ({
+  onChange,
+  onDeleteAccount,
+  onDownloadUserData,
+  onReset,
+  onSubmit,
+  settings,
+}) => {
+  return (
+    <form
+      className="space-y-4 divide-y divide-zinc-300 dark:divide-zinc-600 "
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit();
+      }}
+    >
+      <section className="w-full px-6 py-4">
+        <h2 className="flex items-center gap-4 text-lg font-semibold tracking-wide md:text-2xl">
+          <FeatherIcon icon="smile" />
+          <span>Reactions</span>
+        </h2>
+        <div className="pt-4 space-y-2">
+          <fieldset className="flex items-center justify-between">
+            <label htmlFor="setting-reaction" className="mb-0 md:text-lg">
+              Default Reaction
+            </label>
+            <select
+              name="defaultReaction"
+              id="setting-reaction"
+              className="text-sm rounded dark:bg-zinc-800"
+              onChange={(e) =>
+                onChange({ ...settings, reaction: e.target.value as Reaction })
+              }
+              value={settings.reaction}
+            >
+              <option value={Reaction.SMILE}>😊 Smile</option>
+              <option value={Reaction.JOY}>😂 Laughing</option>
+              <option value={Reaction.HEART_EYES}>😍 Heart Eyes</option>
+              <option value={Reaction.DOWNCAST}>🙁 Downcast</option>
+              <option value={Reaction.THUMBS_UP}>👍 Thumbs Up</option>
+              <option value={Reaction.PINCHED_FINGERS}>
+                🤌 Pinched Fingers
+              </option>
+              <option value={Reaction.FIRE}>🔥 Fire</option>
+            </select>
+          </fieldset>
+          <fieldset className="flex items-center justify-between pr-2">
+            <label htmlFor="setting-cat-mode" className="mb-0 md:text-lg">
+              Use Cat Mode
+            </label>
+            <Switch
+              checked={settings.catMode}
+              id="setting-cat-mode"
+              onChange={(isChecked) =>
+                onChange({ ...settings, catMode: isChecked })
+              }
+              className={`${
+                settings.catMode ? "bg-violet-900" : "bg-violet-700"
+              }
+    relative inline-flex h-[24px] w-[48px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+            >
+              <span className="sr-only">Use setting</span>
+              <span
+                aria-hidden="true"
+                className={`${
+                  settings.catMode ? "translate-x-[24px]" : "translate-x-0"
+                }
+      pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+              />
+            </Switch>
+          </fieldset>
+        </div>
+      </section>
+      <section className="w-full px-6 py-4">
+        <h2 className="flex items-center gap-4 text-lg font-semibold tracking-wide md:text-2xl">
+          <FeatherIcon icon="eye" />
+          <span>Appearance</span>
+        </h2>
+        <div className="pt-4 space-y-2">
+          <fieldset className="flex items-center justify-between">
+            <label htmlFor="setting-theme" className="mb-0 md:text-lg">
+              Theme
+            </label>
+            <select
+              name="theme"
+              id="setting-theme"
+              className="text-sm rounded dark:bg-zinc-800"
+              value={settings.theme}
+              onChange={(e) =>
+                onChange({ ...settings, theme: e.target.value as Theme })
+              }
+            >
+              <option value={Theme.AUTO}>Automatic</option>
+              <option value={Theme.LIGHT}>Light</option>
+              <option value={Theme.DARK}>Dark</option>
+            </select>
+          </fieldset>
+          <fieldset className="flex items-center justify-between pr-2">
+            <label
+              htmlFor="setting-relative-timestamp"
+              className="mb-0 md:text-lg"
+            >
+              Relative Timestamps
+            </label>
+            <Switch
+              checked={settings.relativeTimestamps}
+              id="setting-relative-timestamp"
+              onChange={(isChecked) =>
+                onChange({ ...settings, relativeTimestamps: isChecked })
+              }
+              className={`${
+                settings.relativeTimestamps ? "bg-violet-900" : "bg-violet-700"
+              }
+    relative inline-flex h-[24px] w-[48px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+            >
+              <span className="sr-only">Use setting</span>
+              <span
+                aria-hidden="true"
+                className={`${
+                  settings.relativeTimestamps
+                    ? "translate-x-[24px]"
+                    : "translate-x-0"
+                }
+      pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+              />
+            </Switch>
+          </fieldset>
+        </div>
+      </section>
+      <section className="w-full px-6 py-4">
+        <h2 className="flex items-center gap-4 text-lg font-semibold tracking-wide md:text-2xl">
+          <FeatherIcon icon="bell" />
+          <span>Notifications</span>
+        </h2>
+        <div className="flex flex-col items-center pt-4 space-y-4">
+          <button
+            type="button"
+            className="flex items-center justify-center w-full max-w-lg gap-2 py-2 mx-auto border rounded border-zinc-300 bg-zinc-200 hover:bg-zinc-300 dark:border-zinc-700 dark:bg-zinc-800 dark:hover:bg-zinc-700"
+          >
+            <FeatherIcon icon="power" size={20} />
+            <span>Enable PWA Notifications</span>
+          </button>
+        </div>
+      </section>
+      <section className="w-full px-6 py-4">
+        <h2 className="flex items-center gap-4 text-lg font-semibold tracking-wide md:text-2xl">
+          <FeatherIcon icon="user" />
+          <span>Profile</span>
+        </h2>
+        <div className="flex flex-col items-center pt-4 space-y-4">
+          <button
+            type="button"
+            className="flex items-center justify-center w-full max-w-lg gap-2 py-2 mx-auto border rounded border-zinc-300 bg-zinc-200 hover:bg-zinc-300 dark:border-zinc-700 dark:bg-zinc-800 dark:hover:bg-zinc-700"
+          >
+            <FeatherIcon icon="download" size={20} />
+            <span>Download User Data</span>
+          </button>
+          <button
+            type="button"
+            className="flex items-center justify-center w-full max-w-lg gap-2 py-2 mx-auto border rounded border-rose-300 bg-rose-200 hover:bg-rose-300 dark:border-rose-700 dark:bg-rose-800 dark:hover:bg-rose-700"
+          >
+            <FeatherIcon icon="trash" size={20} />
+            <span>Delete Account</span>
+          </button>
+        </div>
+      </section>
+      <section className="flex items-center justify-center gap-4 px-6 py-4">
+        <button
+          className="flex items-center justify-center w-auto gap-2 px-4 py-2 border rounded-full border-zinc-300 bg-zinc-200 hover:bg-zinc-300 dark:border-zinc-700 dark:bg-zinc-800 dark:hover:bg-zinc-700"
+          onClick={onReset}
+        >
+          <FeatherIcon icon="refresh-cw" size={20} />
+          <span>Reset</span>
+        </button>
+        <button className="flex items-center justify-center w-auto gap-2 px-4 py-2 text-white border rounded-full border-zinc-800 bg-zinc-900 hover:bg-zinc-800 dark:border-zinc-100 dark:bg-white dark:text-zinc-800 dark:hover:bg-zinc-100">
+          <FeatherIcon icon="save" size={20} />
+          <span>Save Settings</span>
+        </button>
+      </section>
+    </form>
+  );
+};
+
+export default SettingsForm;
--- web/src/pages/settings/index.tsx
+++ web/src/pages/settings/index.tsx
@@ -1,17 +1,40 @@
+import { ProfileSettings, Reaction, Theme } from "@prisma/client";
+import { useAtom, useAtomValue } from "jotai";
+import { useCallback, useEffect, useState } from "react";
+
 import FeatherIcon from "feather-icons-react";
 import Head from "next/head";
 import Navbar from "../../components/Navbar";
 import { NextPage } from "next";
-import { Reaction } from "@prisma/client";
-import { Switch } from "@headlessui/react";
+import SettingsForm from "../../components/forms/Settings";
+import atoms from "../../atoms";
+import { trpc } from "../../utils/trpc";
 import { useRouter } from "next/router";
-import { useState } from "react";
 
 const SettingsPage: NextPage = () => {
-  const [useCatMode, setUseCatMode] = useState(false);
-  const [useRelativeTimestamps, setUseRelativeTimestamps] = useState(true);
-
   const router = useRouter();
+
+  const [settings, setSettings] = useState<Omit<ProfileSettings, "id">>({
+    reaction: Reaction.SMILE,
+    catMode: false,
+    theme: Theme.AUTO,
+    relativeTimestamps: true,
+  });
+  const [storedSettings, setStoredSettings] = useAtom(atoms.settings);
+  const updateSettings = trpc.profileSettings.update.useMutation();
+
+  const handleSubmit = useCallback(async () => {
+    const newSettings = await updateSettings.mutateAsync(settings);
+    setStoredSettings(newSettings);
+  }, [storedSettings, settings]);
+
+  useEffect(() => {
+    if (storedSettings) {
+      const { id: _, ...settingsObj } = storedSettings;
+      setSettings(settingsObj);
+    }
+  }, [storedSettings]);
+
   return (
     <>
       <Head>
@@ -22,143 +45,18 @@ const SettingsPage: NextPage = () => {
         />
       </Head>
 
-      <div className="max-w-2xl min-h-screen py-16 mx-auto space-y-4 overflow-y-scroll divide-y hide-scrollbar divide-zinc-300 dark:divide-zinc-600">
-        <section className="w-full px-6 py-4">
-          <h2 className="flex items-center gap-4 text-lg font-semibold tracking-wide md:text-2xl">
-            <FeatherIcon icon="smile" />
-            <span>Reactions</span>
-          </h2>
-          <div className="pt-4 space-y-2">
-            <fieldset className="flex items-center justify-between">
-              <label htmlFor="setting-reaction" className="mb-0 md:text-lg">
-                Default Reaction
-              </label>
-              <select
-                name="defaultReaction"
-                id="setting-reaction"
-                className="text-sm rounded dark:bg-zinc-800"
-              >
-                <option value={Reaction.SMILE}>😊 Smile</option>
-                <option value={Reaction.JOY}>😂 Laughing</option>
-                <option value={Reaction.HEART_EYES}>😍 Heart Eyes</option>
-                <option value={Reaction.DOWNCAST}>🙁 Downcast</option>
-                <option value={Reaction.THUMBS_UP}>👍 Thumbs Up</option>
-                <option value={Reaction.PINCHED_FINGERS}>
-                  🤌 Pinched Fingers
-                </option>
-                <option value={Reaction.FIRE}>🔥 Fire</option>
-              </select>
-            </fieldset>
-            <fieldset className="flex items-center justify-between pr-2">
-              <label htmlFor="setting-cat-mode" className="mb-0 md:text-lg">
-                Use Cat Mode
-              </label>
-              <Switch
-                checked={useCatMode}
-                id="setting-cat-mode"
-                onChange={setUseCatMode}
-                className={`${useCatMode ? "bg-violet-900" : "bg-violet-700"}
-          relative inline-flex h-[24px] w-[48px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-              >
-                <span className="sr-only">Use setting</span>
-                <span
-                  aria-hidden="true"
-                  className={`${
-                    useCatMode ? "translate-x-[24px]" : "translate-x-0"
-                  }
-            pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
-                />
-              </Switch>
-            </fieldset>
-          </div>
-        </section>
-        <section className="w-full px-6 py-4">
-          <h2 className="flex items-center gap-4 text-lg font-semibold tracking-wide md:text-2xl">
-            <FeatherIcon icon="eye" />
-            <span>Appearance</span>
-          </h2>
-          <div className="pt-4 space-y-2">
-            <fieldset className="flex items-center justify-between">
-              <label htmlFor="setting-theme" className="mb-0 md:text-lg">
-                Theme
-              </label>
-              <select
-                name="theme"
-                id="setting-theme"
-                className="text-sm rounded dark:bg-zinc-800"
-              >
-                <option value="AUTOMATIC">Automatic</option>
-                <option value="LIGHT">Light</option>
-                <option value="DARK">Dark</option>
-              </select>
-            </fieldset>
-            <fieldset className="flex items-center justify-between pr-2">
-              <label
-                htmlFor="setting-relative-timestamp"
-                className="mb-0 md:text-lg"
-              >
-                Relative Timestamps
-              </label>
-              <Switch
-                checked={useRelativeTimestamps}
-                id="setting-relative-timestamp"
-                onChange={setUseRelativeTimestamps}
-                className={`${
-                  useRelativeTimestamps ? "bg-violet-900" : "bg-violet-700"
-                }
-          relative inline-flex h-[24px] w-[48px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-              >
-                <span className="sr-only">Use setting</span>
-                <span
-                  aria-hidden="true"
-                  className={`${
-                    useRelativeTimestamps
-                      ? "translate-x-[24px]"
-                      : "translate-x-0"
-                  }
-            pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
-                />
-              </Switch>
-            </fieldset>
-          </div>
-        </section>
-        <section className="w-full px-6 py-4">
-          <h2 className="flex items-center gap-4 text-lg font-semibold tracking-wide md:text-2xl">
-            <FeatherIcon icon="bell" />
-            <span>Notifications</span>
-          </h2>
-          <div className="flex flex-col items-center pt-4 space-y-4">
-            <button
-              type="button"
-              className="flex items-center justify-center w-full max-w-lg gap-2 py-2 mx-auto border rounded border-zinc-300 bg-zinc-200 hover:bg-zinc-300 dark:border-zinc-700 dark:bg-zinc-800 dark:hover:bg-zinc-700"
-            >
-              <FeatherIcon icon="power" size={20} />
-              <span>Enable PWA Notifications</span>
-            </button>
-          </div>
-        </section>
-        <section className="w-full px-6 py-4">
-          <h2 className="flex items-center gap-4 text-lg font-semibold tracking-wide md:text-2xl">
-            <FeatherIcon icon="user" />
-            <span>Profile</span>
-          </h2>
-          <div className="flex flex-col items-center pt-4 space-y-4">
-            <button
-              type="button"
-              className="flex items-center justify-center w-full max-w-lg gap-2 py-2 mx-auto border rounded border-zinc-300 bg-zinc-200 hover:bg-zinc-300 dark:border-zinc-700 dark:bg-zinc-800 dark:hover:bg-zinc-700"
-            >
-              <FeatherIcon icon="download" size={20} />
-              <span>Download User Data</span>
-            </button>
-            <button
-              type="button"
-              className="flex items-center justify-center w-full max-w-lg gap-2 py-2 mx-auto border rounded border-rose-300 bg-rose-200 hover:bg-rose-300 dark:border-rose-700 dark:bg-rose-800 dark:hover:bg-rose-700"
-            >
-              <FeatherIcon icon="trash" size={20} />
-              <span>Delete Account</span>
-            </button>
-          </div>
-        </section>
+      <div className="max-w-2xl min-h-screen py-16 mx-auto overflow-y-scroll hide-scrollbar ">
+        <SettingsForm
+          onChange={(newSettings) => setSettings(newSettings)}
+          onDeleteAccount={console.log}
+          onDownloadUserData={console.log}
+          onReset={() => {
+            console.log(storedSettings);
+            if (storedSettings) setSettings({ ...storedSettings });
+          }}
+          onSubmit={handleSubmit}
+          settings={settings}
+        />
       </div>
 
       <Navbar
