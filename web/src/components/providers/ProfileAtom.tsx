bbdc58c9aff7458ab8a20e5bf3af954b472cbda1
Devin Sharpe <devinrsharpe@gmail.com>
2023-04-05 16:31:49 -0400

    feat: make settings atom persistent and correct effect

--- web/src/components/providers/ProfileAtom.tsx
+++ web/src/components/providers/ProfileAtom.tsx
@@ -2,23 +2,23 @@ import atoms from "../../atoms";
 import { trpc } from "../../utils/trpc";
 import { useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useSetAtom } from "jotai";
+import { useAtom } from "jotai";
+import { useRouter } from "next/router";
 
 const ProfileAtomProvider = () => {
-  const setProfile = useSetAtom(atoms.profile);
-  const setSettings = useSetAtom(atoms.settings);
+  const [profileAtom, setProfile] = useAtom(atoms.profile);
+  const [settingsAtom, setSettings] = useAtom(atoms.settings);
   const session = useSession();
   const profile = trpc.profiles.getProfile.useQuery(undefined, {
     enabled: session.status === "authenticated",
   });
-  const setModal = useSetAtom(atoms.modal);
 
   useEffect(() => {
     if (profile.data) {
       setProfile(profile.data.profile || undefined);
       setSettings(profile.data.settings || undefined);
     }
-  }, [profile, setModal, setProfile]);
+  }, [profile.data, setProfile]);
   return null;
 };
 
