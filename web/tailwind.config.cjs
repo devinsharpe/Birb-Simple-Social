aede3f66ae2f4f079b923f5d0a22ac002d67d060
Devin Sharpe <devinrsharpe@gmail.com>
2023-04-05 18:25:05 -0400

    chore: change tailwind dark mode strategy

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,7 +2,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  darkMode: "media",
+  darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
