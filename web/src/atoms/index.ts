7dcae05dc6fb3efc0166abdacc8293305992882a
Devin Sharpe <devinrsharpe@gmail.com>
2023-04-08 02:54:12 -0400

    feat: add profileReactions atom

--- web/src/atoms/index.ts
+++ web/src/atoms/index.ts
@@ -1,18 +1,21 @@
-import type { Profile, ProfileSettings } from "@prisma/client";
+import { Profile, ProfileReaction, ProfileSettings } from "@prisma/client";
 
 import { atom } from "jotai";
 
 const modalAtom = atom<string | undefined>(undefined);
 const profileAtom = atom<Profile | undefined>(undefined);
+const reactionsAtom = atom<ProfileReaction[]>([]);
 const settingsAtom = atom<ProfileSettings | undefined>(undefined);
 
 modalAtom.debugLabel = "modal";
 profileAtom.debugLabel = "profile";
+reactionsAtom.debugLabel = "reactions";
 settingsAtom.debugLabel = "settings";
 
 const atoms = {
   modal: modalAtom,
   profile: profileAtom,
+  reactions: reactionsAtom,
   settings: settingsAtom,
 };
 
