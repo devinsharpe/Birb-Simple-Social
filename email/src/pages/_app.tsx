f5b512eed80bd8be18f41f669c060190d4468cc3
Devin Sharpe <devinrsharpe@gmail.com>
2023-04-27 02:54:05 -0400

    fix: add noindex meta tag to email and moderation

--- email/src/pages/_app.tsx
+++ email/src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
