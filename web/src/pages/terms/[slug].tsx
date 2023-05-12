14186d776bac7aec9b9a945ba85f95c5381d755f
Devin Sharpe <devinrsharpe@gmail.com>
2023-05-11 20:45:25 -0400

    fix: term and privacy style updates

--- web/src/pages/terms/[slug].tsx
+++ web/src/pages/terms/[slug].tsx
@@ -21,7 +21,7 @@ const TermsPage: NextPage<DocsPageProps> = ({ content, data, docs, slug }) => {
           Birb Social Terms & Conditions
         </h1>
       </div>
-      <div className="mx-8 rounded-md bg-zinc-200 p-4 dark:bg-zinc-800">
+      <div className="mx-8 border-y border-zinc-300 py-4 dark:border-zinc-800">
         <p>
           Effective: {new Date(data.effectiveDate * 1000).toLocaleDateString()}
         </p>
