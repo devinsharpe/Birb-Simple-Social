0a4d927cfdce7c15052276c2c3000e7a61b98fdf
Devin Sharpe <devinrsharpe@gmail.com>
2023-05-11 23:58:24 -0400

    feat: add toasts atom

--- web/src/atoms/index.ts
+++ web/src/atoms/index.ts
@@ -1,22 +1,32 @@
 import type { Profile, ProfileReaction, ProfileSettings } from "@prisma/client";
 
 import { atom } from "jotai";
+import type FeatherIcon from "feather-icons-react";
+
+export interface Toast {
+  id: string;
+  icon?: FeatherIcon.Icon;
+  content: string;
+}
 
 const modalAtom = atom<string | undefined>(undefined);
 const profileAtom = atom<Profile | undefined>(undefined);
 const reactionsAtom = atom<ProfileReaction[]>([]);
 const settingsAtom = atom<ProfileSettings | undefined>(undefined);
+const toastsAtom = atom<Toast[]>([]);
 
 modalAtom.debugLabel = "modal";
 profileAtom.debugLabel = "profile";
 reactionsAtom.debugLabel = "reactions";
 settingsAtom.debugLabel = "settings";
+toastsAtom.debugLabel = "toasts";
 
 const atoms = {
   modal: modalAtom,
   profile: profileAtom,
   reactions: reactionsAtom,
   settings: settingsAtom,
+  toasts: toastsAtom,
 };
 
 export default atoms;
