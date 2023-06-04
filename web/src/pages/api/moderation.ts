be72c1b6900eb6ea20b9c28093501aef7bd4e300
Devin Sharpe <devinrsharpe@gmail.com>
2023-06-04 02:06:44 -0400

    feat: implement drizzle adapter for next auth

--- web/src/pages/api/moderation.ts
+++ web/src/pages/api/moderation.ts
@@ -1,7 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { PostReviewStatus, Visibility } from "~/server/db/schema/enums";
+import { comments, posts } from "~/server/db/schema";
+
+// import { prisma } from "../../server/db/client";
+// import { PostReviewStatus, Visibility } from "@prisma/client";
+import db from "~/server/db";
+import { eq } from "drizzle-orm";
 import { z } from "zod";
-import { prisma } from "../../server/db/client";
-import { PostReviewStatus, Visibility } from "@prisma/client";
 
 const bodySchema = z.array(
   z.object({
@@ -17,32 +22,49 @@ export default async function handler(
 ) {
   if (req.method === "POST") {
     const body = bodySchema.parse(req.body);
-    console.log(body);
     for (const result of body) {
-      const payload: {
-        where: {
-          id: string;
-        };
-        data: {
-          autoReviewedAt: Date;
-          reviewStatus: PostReviewStatus;
-          visibility?: Visibility;
-        };
+      const payloadData: {
+        autoReviewedAt: Date;
+        reviewStatus: PostReviewStatus;
+        visibility?: Visibility;
       } = {
-        where: {
-          id: result.id,
-        },
-        data: {
-          autoReviewedAt: new Date(),
-          reviewStatus: result.hasMatch
-            ? PostReviewStatus.REJECTED_AUTO
-            : PostReviewStatus.APPROVED,
-        },
+        autoReviewedAt: new Date(),
+        reviewStatus: result.hasMatch
+          ? PostReviewStatus.RejectedAuto
+          : PostReviewStatus.Approved,
+        visibility: Visibility.Rejected,
       };
-      if (result.hasMatch) payload.data.visibility = Visibility.REJECTED;
-      console.log(payload);
-      if (result.type === "COMMENT") await prisma.comment.update(payload);
-      else await prisma.post.update(payload);
+      // const payload: {
+      //   where: {
+      //     id: string;
+      //   };
+      //   data: {
+      //     autoReviewedAt: Date;
+      //     reviewStatus: PostReviewStatus;
+      //     visibility?: Visibility;
+      //   };
+      // } = {
+      //   where: {
+      //     id: result.id,
+      //   },
+      //   data: {
+      //     autoReviewedAt: new Date(),
+      //     reviewStatus: result.hasMatch
+      //       ? PostReviewStatus.REJECTED_AUTO
+      //       : PostReviewStatus.APPROVED,
+      //   },
+      // };
+      // if (result.hasMatch) payload.data.visibility = Visibility.REJECTED;
+      // console.log(payload);
+      // if (result.type === "COMMENT") await prisma.comment.update(payload);
+      if (!result.hasMatch) delete payloadData.visibility;
+      if (result.type === "COMMENT")
+        await db
+          .update(comments)
+          .set(payloadData)
+          .where(eq(comments.id, result.id));
+      else
+        await db.update(posts).set(payloadData).where(eq(posts.id, result.id));
     }
     return res.status(200).end();
   }
