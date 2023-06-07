6224cde10358ca6463b648f77839ee1dfcfab8f4
Devin Sharpe <devinrsharpe@gmail.com>
2023-06-06 21:44:32 -0400

    fix: correct imports

--- web/src/server/common/get-server-auth-session.ts
+++ web/src/server/common/get-server-auth-session.ts
@@ -1,7 +1,8 @@
 import { type GetServerSidePropsContext } from "next";
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "../../pages/api/auth/[...nextauth]";
+// import { authOptions } from "../../pages/api/auth/[...nextauth]";
+import authOptions from "~/server/auth/options";
 
 /**
  * Wrapper for getServerSession https://next-auth.js.org/configuration/nextjs
